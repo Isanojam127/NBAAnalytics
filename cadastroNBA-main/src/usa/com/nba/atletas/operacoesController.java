a569c66d2f0ce4a01e251a3d9acb0592a88500e9
lab04 <lab04@lab4-09>
2024-09-23 22:47:04 -0300

    criando a interface

--- cadastroNBA-main/src/usa/com/nba/atletas/operacoesController.java
+++ cadastroNBA-main/src/usa/com/nba/atletas/operacoesController.java
@@ -1,5 +1,35 @@
 package usa.com.nba.atletas;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class operacoesController {
+	
+	@FXML
+	private TextField txf_nome;
+	
+	@FXML
+	private TextField txf_sobrenome;
+	
+	@FXML
+	private TextField txf_idade;
+	
+	@FXML
+	private TextField txf_posi;
+	
+	@FXML
+	private TextField txf_equi;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
 	
 }
